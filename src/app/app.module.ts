cd0ba7ce5e864acedeb2ed3fcaa14a621b0688b9
EduardoGeralde <eduardogeralde83@gmail.com>
2018-03-15 17:36:31 -0300

    chapter 8, 9 ,10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MyFirst } from './my-first/my-first.component';
 import { MyFirst2Component } from './my-first-2/my-first-2.component';
 import { CoursesModule } from './courses/courses.module';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirst,
-    MyFirst2Component
+    MyFirst2Component,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
