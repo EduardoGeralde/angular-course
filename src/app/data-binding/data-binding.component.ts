369beca2cd28fc4c179ae3f4eb336f7a6ac7841c
EduardoGeralde <eduardogeralde83@gmail.com>
2018-03-16 17:48:59 -0300

    chapter 11

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -10,12 +10,28 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem = 'http://lorempixel.com/400/200/sports/';
+  valorInput = '';
+  valorSalvo;
+  isMouseOver: boolean = false;
 
   getValor (){
     return 1;
   }
   getCurtirCurso(){
     return true;
+  }
+  botaoClicado(){
+    alert('Botão Clicado');
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorInput = (<HTMLInputElement>evento.target).value;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
