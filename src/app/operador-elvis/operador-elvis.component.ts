e43356d85c2c774df804c4797d47aa76b8a82a05
EduardoGeralde <eduardogeralde83@gmail.com>
2018-04-10 18:02:38 -0300

    chapter 31

--- src/app/operador-elvis/operador-elvis.component.ts
+++ src/app/operador-elvis/operador-elvis.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperadorElvisComponent implements OnInit {
 
+
+  tarefa: any = {
+    desc: 'Descrição da Tarefa',
+    responsavel: {
+      usuario: null
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
