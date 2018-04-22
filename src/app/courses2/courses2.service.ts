a346f58956b30235654131ea97c7d8216b4aa450
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2018-04-21 22:04:26 -0300

    log added

--- src/app/courses2/courses2.service.ts
+++ src/app/courses2/courses2.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Courses2Service {
 
+    constructor(){
+        console.log('Courses2Service');
+    }
+
     getCourses() {
         return ['Angular 2', 'Java', 'Phonegap'];
     }
