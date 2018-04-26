66911f4013acbf37f9efb1fc57b31d2c33fc8082
EduardoGeralde <eduardogeralde83@gmail.com>
2018-04-26 17:39:09 -0300

    chapter 49

--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -19,6 +20,13 @@ export class ExemplosPipesComponent implements OnInit {
   books: string [] = ['Java', 'Angular 2'];
 
   filtro: string;
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000);
+  });
+
+  valorAsync2 = Observable.interval(2000)
+    .map(valor => 'Valor assíncrono 2');
 
   constructor() { }
 
